4764ffdd5b407a8ae1a6a375b6b05fd76fad471c
Brandon louis <brandonlouisferreira@gmail.com>
2022-11-01 21:21:25 -0300

    change-background-header

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,10 +24,31 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+    const scrollY = window.pageXOffset
 
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        sectionTop = current.offseTop - 50,
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SHOW SCROLL UP ===============*/
 
